a0ff0f87145c39d33e8167c3ccb8f0592119376d
Kamil Kieliszczyk <kamil@kieliszczyk.net>
2015-06-08 10:15:43 +0200

    Remove old and unused scripts for Oh My Zsh

--- oh-my-zsh/custom/envs.zsh
+++
@@ -1,5 +0,0 @@
-ENVS=$HOME/.envs
-if [[ -f $ENVS && -s $ENVS ]]; then
-	source $ENVS
-	echo "$(tput setaf 2)Envs loaded$(tput sgr0)"
-fi;
--- oh-my-zsh/custom/pry.zsh
+++
@@ -1,9 +0,0 @@
-function pry()
-{
-  if [ -e ./config/environment.rb ]
-  then
-    bundle exec pry -r ./config/environment "$@"
-  else
-    bundle exec pry
-  fi
-}

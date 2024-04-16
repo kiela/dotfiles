5dd4f9ae03e02811b6e8637a1a15454c597a94a3
Kamil Kieliszczyk <kamil@kieliszczyk.net>
2024-04-16 14:33:58 +0200

    Refactor variable names in dirrc plugin

--- oh-my-zsh/custom/plugins/dirrc/dirrc.plugin.zsh
+++ oh-my-zsh/custom/plugins/dirrc/dirrc.plugin.zsh
@@ -1,47 +1,47 @@
 __load_dir_links() {
-  local file=$1/.links
+  local __file="$1/.links"
 
-  if [[ -f $file && -s $file ]]; then
+  if [[ -f $__file && -s $__file ]]; then
     echo "$(tput bold)$(tput setaf 6)LINKS:$(tput sgr0)"
     echo "$(tput setaf 6)Check out links stored in .links file!$(tput sgr0)"
   fi;
 }
 
 __load_dir_messages() {
-  local file=$1/.msg
+  local __file="$1/.msg"
 
-  if [[ -f $file && -s $file ]]; then
+  if [[ -f $__file && -s $__file ]]; then
     echo "$(tput bold)$(tput setaf 32)MESSAGE:$(tput sgr0)"
     echo -n "$(tput setaf 32)"
-    cat $file
+    cat $__file
     echo -n "$(tput sgr0)"
   fi;
 }
 
 __load_dir_todos() {
-  local file=$1/.todo
+  local __file="$1/.todo"
 
-  if [[ -f $file && -s $file ]]; then
+  if [[ -f $__file && -s $__file ]]; then
     echo "$(tput bold)$(tput setaf 3)TODO:$(tput sgr0)"
     echo -n "$(tput setaf 3)"
-    cat $file
+    cat $__file
     echo -n "$(tput sgr0)"
   fi;
 }
 
 __load_dir_aliases() {
-  local dir=${1:-$PWD}
-  local filename=${2:-".aliases"}
-  local filepath
+  local __dir=${1:-$PWD}
+  local __filename=${2:-".aliases"}
+  local __filepath
 
-  if [[ -d $dir/$filename ]]; then
-    __load_dir_aliases "$dir/$filename" "_load"
+  if [[ -d $__dir/$__filename ]]; then
+    __load_dir_aliases "$__dir/$__filename" "_load"
   else
-    __filepath=$(__find_dir_file $dir $filename)
+    __filepath=$(__find_dir_file $__dir $__filename)
 
     if [[ $? -eq 0 ]]; then
-      if [[ -f $filepath && -s $filepath ]]; then
-        source $filepath
+      if [[ -f $__filepath && -s $__filepath ]]; then
+        source $__filepath
         echo "$(tput setaf 2)Directory aliases loaded$(tput sgr0)"
       fi
     else
@@ -51,20 +51,20 @@ __load_dir_aliases() {
 }
 
 __load_dir_envs() {
-  local dir=${1:-$PWD}
-  local filename=".env"
-  local filepath
+  local __dir=${1:-$PWD}
+  local __filename=".env"
+  local __filepath
 
-  __filepath=$(__find_dir_file $dir $filename)
+  __filepath=$(__find_dir_file $__dir $__filename)
 
   if [[ $? -eq 0 ]]; then
-    if [[ -f $filepath && -s $filepath ]]; then
+    if [[ -f $__filepath && -s $__filepath ]]; then
       while read i
       do
         if [[ ($i[1] != '#') && (-n $i[1]) ]]; then
           export ${i//[\'\"\`]}
         fi;
-      done < $filepath
+      done < $__filepath
       echo "$(tput setaf 2)Directory ENVs loaded$(tput sgr0)"
     fi
   else
@@ -73,16 +73,16 @@ __load_dir_envs() {
 }
 
 __load_dir_rc() {
-  local dir=${1:-$PWD}
-  local filename=".dirrc"
-  local filepath
+  local __dir=${1:-$PWD}
+  local __filename=".dirrc"
+  local __filepath
 
 
-  __filepath=$(__find_dir_file $dir $filename)
+  __filepath=$(__find_dir_file $__dir $__filename)
 
   if [[ $? -eq 0 ]]; then
-    if [[ -f $filepath && -s $filepath ]]; then
-      source $filepath
+    if [[ -f $__filepath && -s $__filepath ]]; then
+      source $__filepath
       echo "$(tput setaf 2)Directory configuration loaded$(tput sgr0)"
     fi
   else
@@ -91,40 +91,40 @@ __load_dir_rc() {
 }
 
 __find_dir_file() {
-  local start_dir=$1
-  local filename=$2
-  local previous_dir=""
+  local __start_dir=$1
+  local __filename=$2
+  local __previous_dir=""
 
-  if [[ -z "$start_dir" || -z "$filename" ]]; then
+  if [[ -z "$__start_dir" || -z "$__filename" ]]; then
     echo "$(tput setaf 1)WARNING: __find_dir_file(): Required arguments are missing$(tput sgr0)"
     return 1
   fi
 
-  while [[ "$start_dir" != "$previous_dir" ]]; do
-    local filepath="$start_dir/$filename"
+  while [[ "$__start_dir" != "$__previous_dir" ]]; do
+    local __filepath="$__start_dir/$__filename"
 
-    if [[ -f "$filepath" ]]; then
-      echo "$filepath"
+    if [[ -f "$__filepath" ]]; then
+      echo "$__filepath"
       return 0
     else
-      __previous_dir=$start_dir
-      __start_dir=$(dirname "$start_dir")
+      __previous_dir=$__start_dir
+      __start_dir=$(dirname "$__start_dir")
     fi
   done
 }
 
 dirrc() {
-  local dir=${1:-$PWD}
+  local __dir=${1:-$PWD}
 
   # Directory information
-  __load_dir_links $dir
-  __load_dir_messages $dir
-  __load_dir_todos $dir
+  __load_dir_links $__dir
+  __load_dir_messages $__dir
+  __load_dir_todos $__dir
 
   # Directory runtime configuration
-  __load_dir_aliases $dir
-  __load_dir_envs $dir
-  __load_dir_rc $dir
+  __load_dir_aliases $__dir
+  __load_dir_envs $__dir
+  __load_dir_rc $__dir
 }
 
 autoload -U add-zsh-hook

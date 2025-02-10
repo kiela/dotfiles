f8594a8131c7a81cf14a9edd58a0a40c820c160c
Kamil Kieliszczyk <kamil@kieliszczyk.net>
2025-02-10 13:55:13 +0100

    Add shell argument capture reference script

---
+++ bin/func_args_test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Explanation of Different Argument Capture Methods:
+#  - $*: Expands to a single string with all arguments concatenated (losing separation).
+#  - $@: Expands arguments but behaves differently when quoted.
+#  - "*": Expands as a single string.
+#  - "$@": Expands each argument as a separate string (ideal for arrays).
+#  - ("$*"): Stores all arguments as a single element in an array.
+#  - ("$@"): Stores each argument as a separate element in an array (correct choice for preserving argument structure).
+
+func() {
+  #local __args=$*
+  #local __args=$@
+  #local __args="$*"
+  #local __args="$@"
+  #local __args=("$*")
+  local __args=("$@")
+
+  echo "Number of arguments: ${#__args[@]}"
+
+  for arg in "${__args[@]}";
+  do
+    echo "Argument: $arg"
+  done
+}
+
+func "one two" three four
